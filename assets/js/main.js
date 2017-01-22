2b553fafd88ac1c87ef95da02d6136d94f6380b5
Rob M <SpoonyForky@users.noreply.github.com>
2017-01-22 13:57:06 -0500

    Adding SkillSets js

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,3 +118,9 @@
 
 })(jQuery);
 
+$(function() {
+    $('progress').each(function() {
+        var max = $(this).val();
+        $(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+    });
+});
